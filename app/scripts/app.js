4d81922021e558043b37ad5f826c1531213db039
Jens <jens.brimberg@gmail.com>
2016-01-11 15:49:04 +0100

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,8 +34,5 @@ angular
         redirectTo: '/'
       });
     $httpProvider.defaults.useXDomain = true;
-    $httpProvider.defaults.withCredentials = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    $httpProvider.defaults.headers.common["Accept"] = "application/json";
-    $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
   }]);
